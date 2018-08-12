218af627e89b4bddad3f4e3bc51e27435889f5a1
Karuppiah <karuppiah7890@gmail.com>
2018-08-12 21:48:40 +0530

    Add debug mode feature and improve readability with better coloring

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ const shell = require('shelljs')
 const chalk = require('chalk')
 const pLimit = require('p-limit')
 const _ = require('lodash')
+var UI = require('cliui')
 
-const execute = command => {
+const execute = (command, commandID, debug = false) => {
   return new Promise((resolve) => {
-    console.log(chalk.yellow('\nStarted running: '), chalk.blueBright(command));
+    console.log(chalk.yellow('\nStarted running: '), chalk.blueBright(command))
 
-    shell.exec(command, { silent: true }, (exitCode, stdout, stderr) => {
+    const child = shell.exec(command, { silent: true }, (exitCode, stdout, stderr) => {
       resolve({
         command,
         exitCode,
@@ -15,42 +16,77 @@ const execute = command => {
         stderr
       })
     })
+
+    if (debug) {
+      child.stdout.on('data', function (data) {
+        // handling multi-line output from commands
+        const dataLines = data.split('\n')
+        dataLines.forEach((dataLine) => {
+          console.log(`${chalk.yellow(`${commandID}: `)}`, dataLine)
+        })
+      })
+    }
   })
 }
 
-const executeAll = async (commands, numberOfParallelCommands) => {
+const executeAll = async (commands, numberOfParallelCommands, debug = false) => {
   try {
-      console.log(chalk.yellow('The output of all the commands will be shown after running them all'))
-      const limit = pLimit(numberOfParallelCommands)
-      const commandsToRunInParallel = commands.map(command => limit(() => execute(command)))
+    console.log(chalk.yellow('The complete output of all the commands will be shown after running them all'))
 
-      const results = await Promise.all(commandsToRunInParallel)
-      console.log('\nOutput of the commands:\n')
-      results.forEach(result => {
-        console.log(chalk.blueBright('Result of running the below command:'))
-        console.log(`$ ${result.command}\n`)
+    if (debug) {
+      console.log(chalk.yellow('\n***Running in DEBUG mode. Commands will be given an ID and the output of the parallely running\n commands will be shown in an interleaved manner while they are executing, with their command ID prefixed***'))
 
-        if (result.exitCode !== 0) {
-          console.log(chalk.red(`It failed with exit code ${result.exitCode}\n`))
-        } else {
-          console.log(chalk.green(`It ran successfully with no errors! :D\n`))
-        }
+      console.log(chalk.yellow('\n***Running the following commands***'))
 
-        const { stdout, stderr } = result
-        if (!_.isEmpty(_.trim(stdout))) {
-          console.log(chalk.green(`Stdout:`))
-          console.log(stdout)
-        }
-
-        if (!_.isEmpty(_.trim(stderr))) {
-          console.log(chalk.red(`Stderr:`))
-          console.log(stderr)
-        }
+      const commandsTableUI = UI({
+        width: 40
       })
-    } catch (error) {
-      console.log(chalk.red('An error occurred while running the commands in parallel: \n'), error)
-      console.log(chalk.blueBright('\n\nThe next set of commands will still continue to run! :D'))
+
+      commandsTableUI.div(
+        { text: chalk.yellow('CommandID') },
+        { text: chalk.yellow('Command') }
+      )
+
+      commands.forEach((command, commandID) => {
+        commandsTableUI.div(
+          { text: chalk.yellow(commandID) },
+          { text: chalk.blueBright(command) }
+        )
+      })
+
+      console.log(chalk.yellow(commandsTableUI.toString()))
     }
+
+    const limit = pLimit(numberOfParallelCommands)
+    const commandsToRunInParallel = commands.map((command, commandID) => limit(() => execute(command, commandID, debug)))
+
+    const results = await Promise.all(commandsToRunInParallel)
+    console.log('\nOutput of the commands:\n')
+    results.forEach(result => {
+      console.log(chalk.yellow('Result of running the below command:'))
+      console.log(`$ ${chalk.blueBright(result.command)}\n`)
+
+      if (result.exitCode !== 0) {
+        console.log(chalk.red(`It failed with exit code ${result.exitCode}\n`))
+      } else {
+        console.log(chalk.green(`It ran successfully with no errors! :D\n`))
+      }
+
+      const { stdout, stderr } = result
+      if (!_.isEmpty(_.trim(stdout))) {
+        console.log(chalk.green(`Stdout:`))
+        console.log(stdout)
+      }
+
+      if (!_.isEmpty(_.trim(stderr))) {
+        console.log(chalk.red(`Stderr:`))
+        console.log(stderr)
+      }
+    })
+  } catch (error) {
+    console.log(chalk.red('An error occurred while running the commands in parallel: \n'), error)
+    console.log(chalk.blueBright('\n\nThe next set of commands will still continue to run! :D'))
+  }
 }
 
 module.exports = {
