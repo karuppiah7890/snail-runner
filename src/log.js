80460ef3dd656a23c4258e2c9f64893919e65542
Karuppiah <karuppiah7890@gmail.com>
2018-08-17 13:50:49 +0530

    Move out the commands table ui logic to a separate createCommandsTable function

--- src/log.js
+++ src/log.js
@@ -1,11 +1,17 @@
 const chalk = require('chalk')
-var UI = require('cliui')
+const UI = require('cliui')
 
 const logDebugInfo = (commands) => {
   console.log(chalk.yellow('\n***Running in DEBUG mode. Commands will be given an ID and the output of the parallely running\n commands will be shown in an interleaved manner while they are executing, with their command ID prefixed***'))
 
   console.log(chalk.yellow('\n***Running the following commands***'))
 
+  const commandsTable = createCommandsTable(commands)
+
+  console.log(commandsTable, '\n\n')
+}
+
+const createCommandsTable = (commands) => {
   const commandsTableUI = UI()
 
   commandsTableUI.div(
@@ -26,7 +32,7 @@ const logDebugInfo = (commands) => {
     )
   })
 
-  console.log(commandsTableUI.toString(), '\n\n')
+  return commandsTableUI.toString()
 }
 
 module.exports = {
